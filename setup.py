1ac5202a0d364c4f980e42a72364d3d7455c22ae
alexanderkirkup <alexander_kirkup@live.co.uk>
2020-08-12 21:00:16 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='postcode_analysis',
+    version='0.1',
+    description='UK property analysis by postcode.',
+    long_description=readme,
+    author='Alexander Kirkup',
+    url='https://github.com/alexanderkirkup/postcode-analysis',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
