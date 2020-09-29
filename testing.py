3ff399c9612dd8f02f37991367f3617e3d7c07c4
alexanderkirkup <alexander_kirkup@live.co.uk>
2020-09-29 15:39:48 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,22 +3,25 @@ from postcodes import Postcodes
 
 p = Postcodes()
 p.load('postcodes_example.csv')
+# p.load('https://data.london.gov.uk/download/postcode-directory-for-london/62b22f3f-25c5-4dd0-a9eb-06e2d8681ef1/london_postcodes-ons-postcodes-directory-MAY20.csv')
 
-rightmove = Rightmove(p.get_outcodes(), rateLimit=0.2)
+print(len(p.postcodeDict))
 
-params = {
-        'minBedrooms': '1',
-        'maxBedrooms': '1',
-        'radius': '0',
-        'sortType': '1',
-        'propertyTypes': 'flat',
-        #'mustHave': '',
-        'dontShow': 'houseShare,retirement',
-        #'furnishTypes': '',
-        #'keywords': '',
-        }
+# rightmove = Rightmove(p.get_outcodes(), rateLimit=0.2)
 
-rightmove.search_properties(propType='rent', params=params, limit=2)
+# params = {
+#         'minBedrooms': '1',
+#         'maxBedrooms': '1',
+#         'radius': '0',
+#         'sortType': '1',
+#         'propertyTypes': 'flat',
+#         #'mustHave': '',
+#         'dontShow': 'houseShare,retirement',
+#         #'furnishTypes': '',
+#         #'keywords': '',
+#         }
+
+# rightmove.search_properties(propType='rent', params=params, limit=2)
 # rightmove.to_json('results.json')
 
 # rightmove.load_json('results.json')
